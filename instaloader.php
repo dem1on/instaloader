7536164ec774995f8d3e85b0087e0d277c8bfa7f
dem1on <122725982+dem1on@users.noreply.github.com>
2023-01-15 15:12:24 +0100

    Update instaloader.php

--- instaloader.php
+++ instaloader.php
@@ -2,11 +2,6 @@
 echo "WELCOME TO THIS TOOL \n";
 $username=readline("the username of instagram : ");
 
-$path=$username;
-mkdir($path);
-
-
-
 echo "[1] Posts (Images) \n";
 echo "[2] Posts (Videos)\n";
 echo "[3] Posts (Images + Videos)\n";
@@ -14,17 +9,16 @@ echo "[3] Posts (Images + Videos)\n";
 $choice=readline("What do you want download : ");
 
 if ($choice==1) {
-    shell_exec('instaloader '.$username.' --dirname-pattern=./'.$path.' --no-profile-pic --no-metadata-json --no-compress-json --no-captions --no-video-thumbnails --no-videos ');    
+    shell_exec('instaloader '.$username.' --no-profile-pic --no-metadata-json --no-compress-json --no-captions --no-video-thumbnails --no-videos ');    
 } else {
     if ($choice==2) {
-        shell_exec('instaloader '.$username.' --dirname-pattern=./'.$path.' --no-profile-pic --no-metadata-json --no-compress-json --no-captions --no-video-thumbnails --no-pictures ');
+        shell_exec('instaloader '.$username.'  --no-profile-pic --no-metadata-json --no-compress-json --no-captions --no-video-thumbnails --no-pictures ');
     } else {
         if ($choice==3) {
-            shell_exec('instaloader '.$username.' --dirname-pattern=./'.$path.' --no-profile-pic --no-metadata-json --no-compress-json --no-captions --no-video-thumbnails ');
+            shell_exec('instaloader '.$username.'  --no-profile-pic --no-metadata-json --no-compress-json --no-captions --no-video-thumbnails ');
         } else {
             echo 'Make correct choice';
         }
     }
 } 
-
 ?>
